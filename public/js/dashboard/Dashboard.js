a1bbeeae23787eb71371b493bb915746fdd19b75
gongl1 <44885196+gongl1@users.noreply.github.com>
2020-04-29 17:11:10 -0500

    Update Dashboard.js

--- public/js/dashboard/Dashboard.js
+++ public/js/dashboard/Dashboard.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
             if (NOP_VIEWER === null || NOP_VIEWER === undefined) return;
             new Dashboard(NOP_VIEWER, [
                 new BarChart('Material'),
-                new PieChart('Material')
+                new PieChart('Category')
             ])
         }
     });
